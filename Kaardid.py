6a5ebedbfe97718f17faac74d3532c2f404bd5cf
PriitVallap <priitvallap@gmail.com>
2022-01-05 21:47:18 +0200

    Update Kaardid.py

--- Kaardid.py
+++ Kaardid.py
@@ -2,8 +2,14 @@ from itertools import compress, product, combinations
 import random
 from random import sample
 
+mängijaAlustab = True #True, kui alustab mängija, false kui robot.
+loobunud = [False, False]
 mastid = ["d", "s", "c", "h"] # d = ruutu, s = poti, c = risti, h = ärtu
 kaardid = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+punktid = {
+    "mängija": 100,
+    "vastane": 100,
+    "pangas": 0}
 
 kaardipakk = []
 for kaart in kaardid:
@@ -28,11 +34,72 @@ def sega(kaardid):
 segatudPakk, mängijaKäsi, kuldkalaKäsi, ühiskaardid = sega(kaardipakk)
 print("mängijal on:",mängijaKäsi)
 print("vastasel on:",kuldkalaKäsi)
-print("flop on:",ühiskaardid)
 
+def panustamisVoor(ühiskaardid, segatudPakk, mängijaAlustab, punktid, loobunud):
+    print("Laual on:",ühiskaardid,". Pangas on hetkel",punktid["pangas"],"punkti.")
+    if mängijaAlustab:
+        käigulõpp = False
+        mängijaTegevus = input("Kas soovid passida(X) või panustada(B)? ")
+        while not (mängijaTegevus == "X" or mängijaTegevus == "B"):
+            mängijaTegevus = input("Ei saanud aru. Kas soovid passida(X) või panustada(B)?")
+            print(mängijaTegevus)
+            
+        if mängijaTegevus == "X":
+            print("Mängija passib.")
+            robotiTegevus = robotiKäik()
+            if robotiTegevus == "X":
+                print("Vastane passib.")
+                if(len(ühiskaardid) < 5):
+                    uusKaart = segatudPakk.pop()
+                    print("Lauale tuleb uus kaart: ",uusKaart)
+                    ühiskaardid.append(uusKaart)
+                return ühiskaardid, segatudPakk, mängijaAlustab, punktid
+        else:
+            mängijapunkte = punktid["mängija"]
+            panus = panusta(mängijapunkte)
+            print(panus)
+            punktid["mängija"] -= panus
+            punktid["pangas"] += panus
+            print(punktid)
+            
+    else:
+        robotiTegevus = robotiKäik() ## TODO
+        if robotiTegevus == "X":
+            print("Vastane passib.")
+        elif robotiTegevus == "B":
+            robotiPanus        
 
-#def jaga(kaardipakk):
-#    for i in range(0,4,2):
-#        mängijaKäsi.append(kaardipakk[i])
-#        kuldkalaKäsi.append()
-        
+def panusta(maxPunkte):
+    strPanus = input("Sisesta panuse suurus. Lubatud panuse suurus 1 kuni "+str(maxPunkte)+": ")
+    panus = 0
+    while not is_integer(strPanus):
+        strPanus = input("Ei saanud aru. Lubatud panuse suurus 1 kuni "+str(maxPunkte)+": ")
+    if(is_integer(strPanus)):
+        panus = int(strPanus)
+        if panus < 1 or panus > maxPunkte :
+            print("Negatiivne arv või sul ei ole piisavalt punkte")
+            panusta(maxPunkte)
+
+    print(panus)
+    return panus
+                
+#Leitud siit: https://note.nkmk.me/en/python-check-int-float/
+def is_integer(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    else:
+        return float(n).is_integer()
+    
+
+def robotiKäik(): ## TODO
+    return "X"
+
+def mäng(ühiskaardid, segatudPakk, mängijaAlustab, punktid):
+    panustamisVoor(ühiskaardid, segatudPakk, mängijaAlustab, punktid);
+    panustamisVoor(ühiskaardid, segatudPakk, mängijaAlustab, punktid);
+    panustamisVoor(ühiskaardid, segatudPakk, mängijaAlustab, punktid);
+    
+mäng(ühiskaardid, segatudPakk, mängijaAlustab, punktid)
+      
